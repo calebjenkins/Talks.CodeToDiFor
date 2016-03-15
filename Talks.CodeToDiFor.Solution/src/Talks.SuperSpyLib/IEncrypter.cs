1d9df24e00d98a08cc7645b28aaf7b30d0dbcc24
Caleb Jenkins <caleb@calebjenkins.com>
2016-03-14 23:07:42 -0500

    Adding Interfaces

---
+++ Talks.CodeToDiFor.Solution/src/Talks.SuperSpyLib/IEncrypter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Talks.SuperSpyLib
+{
+    public interface IEncrypter
+    {
+        IList<string> Encrypt(IList<string> Current, string Message);
+    }
+}
