0382283d4cfc5d683771d58f50482a47fa15c8e6
Caleb Jenkins <caleb@calebjenkins.com>
2016-03-15 14:47:22 -0500

    Deep Dependencies

--- Talks.CodeToDiFor.Solution/src/Talks.SuperSpyLib/Imp/DataLayer.cs
+++ Talks.CodeToDiFor.Solution/src/Talks.SuperSpyLib/Imp/DataLayer.cs
@@ -8,9 +8,21 @@ namespace Talks.SuperSpyLib.Imp
 {
     public class DataLayer : IDataLayer
     {
+        IEncrypter crypto;
+        ISpyLogger logger;
+
+        public DataLayer(IEncrypter Encrypter, ISpyLogger LogIt)
+        {
+            crypto = Encrypter;
+            logger = LogIt;
+        }
+
         public IList<string> Update(IList<string> Context, string Data)
         {
+            Context = crypto.Encrypt(Context, "Encrypt before putting in DB!");
             Context.Add("DataLayer: " + Data);
+            Context = logger.Log(Context, "Stored in DB");
+
             return Context;
         }
     }
--- Talks.CodeToDiFor.Solution/src/Talks.SuperSpyLib/Imp/MessageSender.cs
+++ Talks.CodeToDiFor.Solution/src/Talks.SuperSpyLib/Imp/MessageSender.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Talks.SuperSpyLib.Data;
 
 namespace Talks.SuperSpyLib.Imp
 {
     public class MessageSender : IMessageSender
     {
+        IEncrypter encrypter;
+        IDataLayer data;
+        ISpyLogger logger;
+
+        
+        public MessageSender(IEncrypter EncryptionYo, ISpyLogger SuperSpyLogger, IDataLayer BigDATA)
+        {
+            encrypter = EncryptionYo;
+            data = BigDATA;
+            logger = SuperSpyLogger;
+        }
         public IList<string> Send(IList<string> Current, string Message)
         {
             Current.Add("MessageSender: " + Message);
+            Current = encrypter.Encrypt(Current, "Secret Message!!");
+           Current = data.Update(Current, "Storing Data!");
+
+            Current = logger.Log(Current, "The Secret Message has been sent!");
+
             return Current;
         }
     }
