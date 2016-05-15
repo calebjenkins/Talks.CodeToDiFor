9a061eb27b6549bc060dd3c3bf6b9b1acb659098
Caleb Jenkins <caleb@calebjenkins.com>
2016-05-15 18:54:17 -0500

    Initial Console app

---
+++ Talks.CodeToDiFor.Solution/Talks.CodeToDiFor.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talks.CodeToDiFor.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("\t* * * Spy Message Sender * * *");
+            Console.Write("\tMessage:");
+            var input = Console.ReadLine();
+
+            
+
+        }
+    }
+}
