889f784704fa444629ef1167114701d0de1787b6
CJenkins2 <calebjenkins@quickenloans.com>
2017-10-04 22:22:15 -0500

    Adding tests

---
+++ Talks.CodeToDiFor.Solution/Talks.C2DF.Tests/BetterApp.LibTests/Rules/GodSaveTheQueenTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Talks.C2DF.BetterApp.Lib.Rules;
+using Talks.C2DF.Interfaces.Models;
+
+namespace Talks.C2DF.Tests.BetterAppLibTests.Rules
+{
+	[TestClass]
+	public class GodSaveTheQueenTest
+	{
+		GodSaveQueenExtendedPriceRule rule;
+		MessageForProcessing validMsg;
+		MessageForProcessing invalidMsg;
+
+		[TestInitialize]
+		public void SetUp()
+		{
+			rule = new GodSaveQueenExtendedPriceRule();
+			validMsg = new MessageForProcessing() { CurrentPrice = 10, Text = "GodSaveTheQueen", Weight = 10 };
+			invalidMsg = new MessageForProcessing() { CurrentPrice = 20, Text = "This is a message", Weight = 30 };
+		}
+
+		[TestMethod]
+		public void Should_Set_as_free_when_applies()
+		{
+			Assert.IsTrue(rule.AppliesTo(validMsg));
+			Assert.IsTrue(0 == rule.Apply(validMsg));
+		}
+
+		[TestMethod]
+		public void Should_not_apply_when_doesnt_apply()
+		{
+			Assert.IsFalse(rule.AppliesTo(invalidMsg));
+		}
+	}
+}
---
+++ Talks.CodeToDiFor.Solution/Talks.C2DF.Tests/BetterApp.LibTests/When_FedExSender_Send_.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Talks.C2DF.BetterApp.Lib;
+using Talks.C2DF.BetterApp.Lib.Logging;
+using Talks.C2DF.Interfaces;
+
+namespace Talks.C2DF.Tests.BetterAppLibTests
+{
+	[TestClass]
+	public class When_FedExSender_Send_
+	{
+		const string expectedText = "Hello World";
+		const string cryptoText = "ENCRYPTED_TEXT";
+
+		[TestMethod]
+		public void Should_Encrypt_First()
+		{
+			var cryptoMock = new Mock<IEncryptHelper>();
+			cryptoMock.Setup(x => x.Encrypt(It.Is<string>((str) => str == expectedText))).Returns(cryptoText).Verifiable();
+
+			var loggerMock = new Mock<IAppLogger>();
+
+			var sut = new FedExSender(cryptoMock.Object, loggerMock.Object);
+			sut.Send(expectedText);
+
+			cryptoMock.VerifyAll();
+		}
+
+		[TestMethod]
+		public void Should_Log_Crypto_Text()
+		{
+			var cryptoMock = new Mock<IEncryptHelper>();
+			cryptoMock.Setup(x => x.Encrypt(It.Is<string>((str) => str == expectedText))).Returns(cryptoText).Verifiable();
+
+			var loggerMock = new Mock<IAppLogger>();
+			loggerMock.Setup(x => x.Info(It.Is<string>((str) => str.Contains(cryptoText)))).Verifiable();
+
+			var sut = new FedExSender(cryptoMock.Object, loggerMock.Object);
+			sut.Send(expectedText);
+
+			cryptoMock.VerifyAll();
+			loggerMock.VerifyAll();
+		}
+	}
+}
