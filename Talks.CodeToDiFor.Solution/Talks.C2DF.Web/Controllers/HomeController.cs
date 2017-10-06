2e1b56e1dc17256f81332a39ae79b39b2410e753
CJenkins2 <calebjenkins@quickenloans.com>
2017-10-05 21:27:25 -0500

    Update Home Controller to send message - and log it

--- Talks.CodeToDiFor.Solution/Talks.C2DF.Web/Controllers/HomeController.cs
+++ Talks.CodeToDiFor.Solution/Talks.C2DF.Web/Controllers/HomeController.cs
@@ -3,28 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Talks.C2DF.BetterApp.Lib.Logging;
 using Talks.C2DF.Interfaces;
 
 namespace Talks.C2DF.Web.Controllers
 {
 	public class HomeController: Controller
 	{
-		//ICostCalculator calculator;
-		//public HomeController(ICostCalculator calculator)
-		//{
-		//	this.calculator = calculator;
-		//}
 
 		ISendingMicroApp sendingApp;
-		public HomeController(ISendingMicroApp sendingApp)
+		IAppLogger _logger;
+
+		public HomeController(ISendingMicroApp sendingApp, IAppLogger logger)
 		{
 			this.sendingApp = sendingApp;
+			_logger = logger;
 		}
 
 		public ActionResult Index()
 		{
-
+			_logger.Debug("Sending Message from MVC App");
 			var result = sendingApp.Send("Hello World!!!DEAL");
+
+			_logger.Debug($"Result: {result.ResultMessage} -- Price: {result.Price} -- Message: {result.Message} ");
 
 			return View();
 		}
