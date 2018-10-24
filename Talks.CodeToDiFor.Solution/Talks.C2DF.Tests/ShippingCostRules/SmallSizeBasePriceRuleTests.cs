1e50c1b3bb718c92cda666c6f1d6bc52f4a0df15
CJenkins2 <calebjenkins@quickenloans.com>
2018-10-23 21:30:08 -0500

    Adding more tests

---
+++ Talks.CodeToDiFor.Solution/Talks.C2DF.Tests/ShippingCostRules/SmallSizeBasePriceRuleTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talks.C2DF.BetterAppLib;
+using Talks.C2DF.BetterAppLib.Rules;
+using Talks.C2DF.Models;
+
+namespace Talks.C2DF.Tests.ShippingCostRules
+{
+	[TestClass]
+	public class SmallSizeBasePriceRuleTests
+	{
+		private SmallSizeBasePriceRule _rule;
+		private MessageForProcessing _msg;
+
+		[TestInitialize]
+		public void SetUp()
+		{
+			_rule = new SmallSizeBasePriceRule();
+		}
+
+		[TestMethod]
+		public void SmallSizeBasePriceRule_should_accept_less_than_5()
+		{
+			_msg = new MessageForProcessing() { Text = "123" };
+		}
+	}
+}
